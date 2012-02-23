8341cb52d62e30f2ca653574d03604b6a124040d
superatmel <superatmel@gmail.com>
2012-02-23 23:07:38 +0100

    Fix variable
    Change-Id: I8fac27b9ba0335b94f4697ed2a7617481f50841d

--- full_galaxysmtd.mk
+++ full_galaxysmtd.mk
@@ -22,7 +22,7 @@
 PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
 
 # Inherit from those products. Most specific first.
-$#(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # This is where we'd set a backup provider if we had one
 #$(call inherit-product, device/sample/products/backup_overlay.mk)
